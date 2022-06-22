f118b8a1b85d8a88db7c549f668125baf4382066
Mohammad Ismail <mohammad2002ismail@gmail.com>
2022-06-22 14:04:29 +0300

    commit

--- backend/utils/authenticateUserPassword.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-
-module.exports.authenticateUserPassword = async (enteredPassword,realPassword)=>{
-    return await bcrypt.compare(enteredPassword,realPassword);
-}
