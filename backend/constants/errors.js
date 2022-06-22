b710b0f4240f2c2ab3af34e550f5a2759963e91b
Mohammad Ismail <mohammad2002ismail@gmail.com>
2022-06-22 14:01:00 +0300

    added constants for errors

---
+++ backend/constants/errors.js
@@ -0,0 +1,14 @@
+module.exports = {
+    EMAIL_DUPLICATE:{
+        message:"Someone has registered using the email",
+        statusCode: 400
+    },
+    INVALID_CREDENTIALS:{
+        message:"Wrong email or password",
+        statusCode:400
+    },
+    DEFAULT:{
+        message:"Something went wrong",
+        statusCode:500
+    }
+}
